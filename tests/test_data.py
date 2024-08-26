2a11afdf5f78275a9479921442bf1f100d2a9fa2
Amaury Louarn <git@homnomnom.fr>
2024-08-26 08:03:28 +0200

    fix data test

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,10 +3,12 @@ import budgie.data
 
 def test_load_users():
   alice = budgie.data.get_user("alice")
-  assert len(alice.accounts) > 0
+  assert alice is not None
+  assert alice.accounts.contains("Assets:US:BofA:Checking")
 
   bob = budgie.data.get_user("bob")
-  assert len(bob.accounts) > 0
+  assert bob is not None
+  assert alice.accounts.contains("Assets:US:BofA:Checking")
 
   carl = budgie.data.get_user("carl")
   assert carl is None
